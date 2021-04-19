411f7f45e27e269665d4a1413c7c631ceb238b3c
Amirjan <50082485+erxdev@users.noreply.github.com>
2021-04-19 14:23:26 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace KeystrokesByErx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panelW = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelW = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelA = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelA = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelS = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelS = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelD = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelD = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelSpace = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelSpace = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelLMB = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelLMB = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelRMB = new Bunifu.UI.WinForms.BunifuPanel();
+            this.labelRMB = new Bunifu.UI.WinForms.BunifuLabel();
+            this.panelW.SuspendLayout();
+            this.panelA.SuspendLayout();
+            this.panelS.SuspendLayout();
+            this.panelD.SuspendLayout();
+            this.panelSpace.SuspendLayout();
+            this.panelLMB.SuspendLayout();
+            this.panelRMB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panelW
+            // 
+            this.panelW.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelW.BackgroundColor = System.Drawing.Color.Black;
+            this.panelW.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelW.BackgroundImage")));
+            this.panelW.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelW.BorderColor = System.Drawing.Color.Transparent;
+            this.panelW.BorderRadius = 0;
+            this.panelW.BorderThickness = 3;
+            this.panelW.Controls.Add(this.labelW);
+            this.panelW.Location = new System.Drawing.Point(51, 1);
+            this.panelW.Margin = new System.Windows.Forms.Padding(1);
+            this.panelW.Name = "panelW";
+            this.panelW.ShowBorders = true;
+            this.panelW.Size = new System.Drawing.Size(50, 50);
+            this.panelW.TabIndex = 0;
+            // 
+            // labelW
+            // 
+            this.labelW.AllowParentOverrides = false;
+            this.labelW.AutoEllipsis = false;
+            this.labelW.AutoSize = false;
+            this.labelW.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelW.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelW.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelW.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelW.Location = new System.Drawing.Point(0, 0);
+            this.labelW.Name = "labelW";
+            this.labelW.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelW.Size = new System.Drawing.Size(50, 50);
+            this.labelW.TabIndex = 1;
+            this.labelW.Text = "W";
+            this.labelW.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelW.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelA
+            // 
+            this.panelA.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelA.BackgroundColor = System.Drawing.Color.Black;
+            this.panelA.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelA.BackgroundImage")));
+            this.panelA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelA.BorderColor = System.Drawing.Color.Transparent;
+            this.panelA.BorderRadius = 0;
+            this.panelA.BorderThickness = 3;
+            this.panelA.Controls.Add(this.labelA);
+            this.panelA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelA.Location = new System.Drawing.Point(1, 51);
+            this.panelA.Name = "panelA";
+            this.panelA.ShowBorders = true;
+            this.panelA.Size = new System.Drawing.Size(50, 50);
+            this.panelA.TabIndex = 1;
+            // 
+            // labelA
+            // 
+            this.labelA.AllowParentOverrides = false;
+            this.labelA.AutoEllipsis = false;
+            this.labelA.AutoSize = false;
+            this.labelA.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelA.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelA.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelA.Location = new System.Drawing.Point(0, 0);
+            this.labelA.Margin = new System.Windows.Forms.Padding(1);
+            this.labelA.Name = "labelA";
+            this.labelA.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelA.Size = new System.Drawing.Size(50, 50);
+            this.labelA.TabIndex = 0;
+            this.labelA.Text = "A";
+            this.labelA.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelA.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelS
+            // 
+            this.panelS.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelS.BackgroundColor = System.Drawing.Color.Black;
+            this.panelS.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelS.BackgroundImage")));
+            this.panelS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelS.BorderColor = System.Drawing.Color.Transparent;
+            this.panelS.BorderRadius = 0;
+            this.panelS.BorderThickness = 3;
+            this.panelS.Controls.Add(this.labelS);
+            this.panelS.Location = new System.Drawing.Point(51, 51);
+            this.panelS.Name = "panelS";
+            this.panelS.ShowBorders = true;
+            this.panelS.Size = new System.Drawing.Size(50, 50);
+            this.panelS.TabIndex = 1;
+            // 
+            // labelS
+            // 
+            this.labelS.AllowParentOverrides = false;
+            this.labelS.AutoEllipsis = false;
+            this.labelS.AutoSize = false;
+            this.labelS.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelS.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelS.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelS.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelS.Location = new System.Drawing.Point(0, 0);
+            this.labelS.Margin = new System.Windows.Forms.Padding(1);
+            this.labelS.Name = "labelS";
+            this.labelS.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelS.Size = new System.Drawing.Size(50, 50);
+            this.labelS.TabIndex = 0;
+            this.labelS.Text = "S";
+            this.labelS.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelS.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelD
+            // 
+            this.panelD.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelD.BackgroundColor = System.Drawing.Color.Black;
+            this.panelD.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelD.BackgroundImage")));
+            this.panelD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelD.BorderColor = System.Drawing.Color.Transparent;
+            this.panelD.BorderRadius = 0;
+            this.panelD.BorderThickness = 3;
+            this.panelD.Controls.Add(this.labelD);
+            this.panelD.Location = new System.Drawing.Point(101, 51);
+            this.panelD.Name = "panelD";
+            this.panelD.ShowBorders = true;
+            this.panelD.Size = new System.Drawing.Size(50, 50);
+            this.panelD.TabIndex = 1;
+            // 
+            // labelD
+            // 
+            this.labelD.AllowParentOverrides = false;
+            this.labelD.AutoEllipsis = false;
+            this.labelD.AutoSize = false;
+            this.labelD.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelD.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelD.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelD.Location = new System.Drawing.Point(0, 0);
+            this.labelD.Margin = new System.Windows.Forms.Padding(1);
+            this.labelD.Name = "labelD";
+            this.labelD.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelD.Size = new System.Drawing.Size(50, 50);
+            this.labelD.TabIndex = 0;
+            this.labelD.Text = "D";
+            this.labelD.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelD.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelSpace
+            // 
+            this.panelSpace.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelSpace.BackgroundColor = System.Drawing.Color.Black;
+            this.panelSpace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelSpace.BackgroundImage")));
+            this.panelSpace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelSpace.BorderColor = System.Drawing.Color.Transparent;
+            this.panelSpace.BorderRadius = 0;
+            this.panelSpace.BorderThickness = 3;
+            this.panelSpace.Controls.Add(this.labelSpace);
+            this.panelSpace.Location = new System.Drawing.Point(1, 101);
+            this.panelSpace.Name = "panelSpace";
+            this.panelSpace.ShowBorders = true;
+            this.panelSpace.Size = new System.Drawing.Size(150, 36);
+            this.panelSpace.TabIndex = 1;
+            // 
+            // labelSpace
+            // 
+            this.labelSpace.AllowParentOverrides = false;
+            this.labelSpace.AutoEllipsis = false;
+            this.labelSpace.AutoSize = false;
+            this.labelSpace.CursorType = null;
+            this.labelSpace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelSpace.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelSpace.Location = new System.Drawing.Point(0, 0);
+            this.labelSpace.Margin = new System.Windows.Forms.Padding(1);
+            this.labelSpace.Name = "labelSpace";
+            this.labelSpace.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelSpace.Size = new System.Drawing.Size(150, 36);
+            this.labelSpace.TabIndex = 1;
+            this.labelSpace.Text = "_____";
+            this.labelSpace.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSpace.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelLMB
+            // 
+            this.panelLMB.BackgroundColor = System.Drawing.Color.Black;
+            this.panelLMB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLMB.BackgroundImage")));
+            this.panelLMB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLMB.BorderColor = System.Drawing.Color.Transparent;
+            this.panelLMB.BorderRadius = 0;
+            this.panelLMB.BorderThickness = 3;
+            this.panelLMB.Controls.Add(this.labelLMB);
+            this.panelLMB.Location = new System.Drawing.Point(1, 136);
+            this.panelLMB.Name = "panelLMB";
+            this.panelLMB.ShowBorders = true;
+            this.panelLMB.Size = new System.Drawing.Size(76, 36);
+            this.panelLMB.TabIndex = 2;
+            // 
+            // labelLMB
+            // 
+            this.labelLMB.AllowParentOverrides = false;
+            this.labelLMB.AutoEllipsis = false;
+            this.labelLMB.AutoSize = false;
+            this.labelLMB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelLMB.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelLMB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelLMB.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelLMB.Location = new System.Drawing.Point(0, 0);
+            this.labelLMB.Margin = new System.Windows.Forms.Padding(1);
+            this.labelLMB.Name = "labelLMB";
+            this.labelLMB.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelLMB.Size = new System.Drawing.Size(76, 36);
+            this.labelLMB.TabIndex = 4;
+            this.labelLMB.Text = "LMB";
+            this.labelLMB.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelLMB.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // panelRMB
+            // 
+            this.panelRMB.BackgroundColor = System.Drawing.Color.Black;
+            this.panelRMB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelRMB.BackgroundImage")));
+            this.panelRMB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelRMB.BorderColor = System.Drawing.Color.Transparent;
+            this.panelRMB.BorderRadius = 0;
+            this.panelRMB.BorderThickness = 3;
+            this.panelRMB.Controls.Add(this.labelRMB);
+            this.panelRMB.Location = new System.Drawing.Point(76, 136);
+            this.panelRMB.Name = "panelRMB";
+            this.panelRMB.ShowBorders = true;
+            this.panelRMB.Size = new System.Drawing.Size(76, 36);
+            this.panelRMB.TabIndex = 4;
+            // 
+            // labelRMB
+            // 
+            this.labelRMB.AllowParentOverrides = false;
+            this.labelRMB.AutoEllipsis = false;
+            this.labelRMB.AutoSize = false;
+            this.labelRMB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelRMB.CursorType = System.Windows.Forms.Cursors.Default;
+            this.labelRMB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelRMB.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.labelRMB.Location = new System.Drawing.Point(0, 0);
+            this.labelRMB.Margin = new System.Windows.Forms.Padding(1);
+            this.labelRMB.Name = "labelRMB";
+            this.labelRMB.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelRMB.Size = new System.Drawing.Size(76, 36);
+            this.labelRMB.TabIndex = 0;
+            this.labelRMB.Text = "RMB";
+            this.labelRMB.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelRMB.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(153, 175);
+            this.ControlBox = false;
+            this.Controls.Add(this.panelRMB);
+            this.Controls.Add(this.panelLMB);
+            this.Controls.Add(this.panelSpace);
+            this.Controls.Add(this.panelD);
+            this.Controls.Add(this.panelS);
+            this.Controls.Add(this.panelA);
+            this.Controls.Add(this.panelW);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnApplicationExit);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panelW.ResumeLayout(false);
+            this.panelA.ResumeLayout(false);
+            this.panelS.ResumeLayout(false);
+            this.panelD.ResumeLayout(false);
+            this.panelSpace.ResumeLayout(false);
+            this.panelLMB.ResumeLayout(false);
+            this.panelRMB.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private Bunifu.UI.WinForms.BunifuPanel panelW;
+        private Bunifu.UI.WinForms.BunifuPanel panelA;
+        private Bunifu.UI.WinForms.BunifuPanel panelS;
+        private Bunifu.UI.WinForms.BunifuPanel panelD;
+        private Bunifu.UI.WinForms.BunifuPanel panelSpace;
+        private Bunifu.UI.WinForms.BunifuPanel panelLMB;
+        private Bunifu.UI.WinForms.BunifuPanel panelRMB;
+        private Bunifu.UI.WinForms.BunifuLabel labelW;
+        private Bunifu.UI.WinForms.BunifuLabel labelA;
+        private Bunifu.UI.WinForms.BunifuLabel labelS;
+        private Bunifu.UI.WinForms.BunifuLabel labelD;
+        private Bunifu.UI.WinForms.BunifuLabel labelSpace;
+        private Bunifu.UI.WinForms.BunifuLabel labelLMB;
+        private Bunifu.UI.WinForms.BunifuLabel labelRMB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Linq;
+using Utilities;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+using GlobalLowLevelHooks;
+
+namespace KeystrokesByErx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Color HsvToRgb(float h, float s, float v)
+        {
+            int i;
+            float f, p, q, t;
+
+            if (s < float.Epsilon)
+            {
+                int c = (int)(v * 255);
+                return Color.FromArgb(c, c, c);
+            }
+
+            h /= 60;
+            i = (int)Math.Floor(h);
+            f = h - i;
+            p = v * (1 - s);
+            q = v * (1 - s * f);
+            t = v * (1 - s * (1 - f));
+
+            float r, g, b;
+            switch (i)
+            {
+                case 0: r = v; g = t; b = p; break;
+                case 1: r = q; g = v; b = p; break;
+                case 2: r = p; g = v; b = t; break;
+                case 3: r = p; g = q; b = v; break;
+                case 4: r = t; g = p; b = v; break;
+                default: r = v; g = p; b = q; break;
+            }
+
+            return Color.FromArgb((int)(r * 255), (int)(g * 255), (int)(b * 255));
+        }
+        float h = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            h++;
+            if (h >= 360) h = 0;
+            panelW.BorderColor = HsvToRgb(h, 1f, 1f);
+            labelW.ForeColor = HsvToRgb(h, 1f, 1f);
+            panelA.BorderColor = HsvToRgb(h, 0.8f, 1f);
+            labelA.ForeColor = HsvToRgb(h, 0.8f, 1f);
+            panelS.BorderColor = HsvToRgb(h, 0.8f, 1f);
+            labelS.ForeColor = HsvToRgb(h, 0.8f, 1f);
+            panelD.BorderColor = HsvToRgb(h, 0.8f, 1f);
+            labelD.ForeColor = HsvToRgb(h, 0.8f, 1f);
+            panelSpace.BorderColor = HsvToRgb(h, 0.6f, 1f);
+            labelSpace.ForeColor = HsvToRgb(h, 0.6f, 1f);
+            panelLMB.BorderColor = HsvToRgb(h, 0.4f, 1f);
+            labelLMB.ForeColor = HsvToRgb(h, 0.4f, 1f);
+            panelRMB.BorderColor = HsvToRgb(h, 0.4f, 1f);
+            labelRMB.ForeColor = HsvToRgb(h, 0.4f, 1f);
+        }
+        KeyboardHook keyboardHook = new KeyboardHook();
+        MouseHook mouseHook = new MouseHook();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+            mouseHook.LeftButtonDown += new MouseHook.MouseHookCallback(mouseHook_LeftButtonDown);
+            mouseHook.LeftButtonUp += new MouseHook.MouseHookCallback(mouseHook_LeftButtonUp);
+            mouseHook.RightButtonDown += new MouseHook.MouseHookCallback(mouseHook_RightButtonDown);
+            mouseHook.RightButtonUp += new MouseHook.MouseHookCallback(mouseHook_RightButtonUp);
+            mouseHook.Install();
+            keyboardHook.KeyDown += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyDown);
+            keyboardHook.KeyUp += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyUp);
+            keyboardHook.Install();
+        }
+
+        
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+        /*private void mouseHook_MouseMove(MouseHook.)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] KeyDown Event {" + key.ToString() + "}");
+        }*/
+        private void keyboardHook_KeyDown(KeyboardHook.VKeys key)
+        {
+            if (key == KeyboardHook.VKeys.KEY_W)
+            {
+                panelW.BackgroundColor = System.Drawing.Color.White;
+            }
+            if (key == KeyboardHook.VKeys.KEY_A)
+            {
+                panelA.BackgroundColor = System.Drawing.Color.White;
+            }
+            if (key == KeyboardHook.VKeys.KEY_S)
+            {
+                panelS.BackgroundColor = System.Drawing.Color.White;
+            }
+            if (key == KeyboardHook.VKeys.KEY_D)
+            {
+                panelD.BackgroundColor = System.Drawing.Color.White;
+            }
+            if (key == KeyboardHook.VKeys.SPACE)
+            {
+                panelSpace.BackgroundColor = System.Drawing.Color.White;
+            }
+        }
+        private void keyboardHook_KeyUp(KeyboardHook.VKeys key)
+        {
+            if (key == KeyboardHook.VKeys.KEY_W)
+            {
+                panelW.BackgroundColor = System.Drawing.Color.Black;
+            }
+            if (key == KeyboardHook.VKeys.KEY_A)
+            {
+                panelA.BackgroundColor = System.Drawing.Color.Black;
+            }
+            if (key == KeyboardHook.VKeys.KEY_S)
+            {
+                panelS.BackgroundColor = System.Drawing.Color.Black;
+            }
+            if (key == KeyboardHook.VKeys.KEY_D)
+            {
+                panelD.BackgroundColor = System.Drawing.Color.Black;
+            }
+            if (key == KeyboardHook.VKeys.SPACE)
+            {
+                panelSpace.BackgroundColor = System.Drawing.Color.Black;
+            }
+        }
+        private void mouseHook_LeftButtonDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            panelLMB.BackgroundColor = System.Drawing.Color.White;
+        }
+        private void mouseHook_LeftButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            panelLMB.BackgroundColor = System.Drawing.Color.Black;
+        }
+        private void mouseHook_RightButtonDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            panelRMB.BackgroundColor = System.Drawing.Color.White;
+        }
+        private void mouseHook_RightButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            panelRMB.BackgroundColor = System.Drawing.Color.Black;
+        }
+
+        private void OnApplicationExit(object sender, FormClosingEventArgs e)
+        {
+
+            keyboardHook.KeyDown -= new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyDown);
+            keyboardHook.KeyUp -= new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyUp);
+            keyboardHook.Uninstall();
+            mouseHook.LeftButtonDown -= new MouseHook.MouseHookCallback(mouseHook_LeftButtonDown);
+            mouseHook.LeftButtonUp -= new MouseHook.MouseHookCallback(mouseHook_LeftButtonUp);
+            mouseHook.RightButtonDown -= new MouseHook.MouseHookCallback(mouseHook_RightButtonDown);
+            mouseHook.RightButtonUp -= new MouseHook.MouseHookCallback(mouseHook_RightButtonUp);
+            mouseHook.Uninstall();
+        }
+    }
+}
